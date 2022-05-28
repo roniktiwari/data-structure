b0d1b4d228054c0061bb81d10c9bc8c5327300d6
Vipul Tiwari <48161515+vr-vipultiwari@users.noreply.github.com>
2022-05-28 22:51:12 +0530

    more optimize form

--- Recursion/MazePathWithJumps.java
+++ Recursion/MazePathWithJumps.java
@@ -8,16 +8,11 @@ public class MazePathWithJumps {
 
     public static void main(String[] args) {
         // suppose we have a maze ( 3*4 )
-          List<String> gtp =  getThePaths(1,1,4,4);
+          List<String> gtp =  getThePaths(1,1,2,2);
           System.out.println(gtp);
     }
 
     private static List<String> getThePaths(int sx, int sy, int dx, int dy) {
-
-         if(sx>dx || sy> dy){
-             ArrayList<String> stx = new ArrayList<>( );
-             return stx ;
-         }
 
 
         if((sx==dx ) && (sy==dy)) {
@@ -26,20 +21,20 @@ public class MazePathWithJumps {
             return stx ;
         }
         List<String> res = new ArrayList<>();
-        for(int i = 1 ; i<dy && sy<dy ; i++) {
+        for(int i = 1 ; i<=dy-sy ; i++) {
             List<String> paths = getThePaths(sx, sy + i, dx, dy);
             for(String p : paths){
                 res.add("h"+i+p);
             }
         }
-        for(int i = 1 ; i<dx && sx<dx; i++) {
+        for(int i = 1 ; i<=dx-sx ; i++) {
             List<String> paths = getThePaths(sx+i, sy, dx, dy);
             for(String p : paths){
                 res.add("v"+i+p);
             }
         }
 
-        for(int i = 1 ; i<dx  ; i++) {
+        for(int i = 1 ; i<=dx-sx ; i++) {
             List<String> paths = getThePaths(sx+i, sy+i, dx, dy);
             for(String p : paths){
                 res.add("d"+i+p);
